7bfcfd2e506730247ad2e18991ea0a13c658ddba
Arthur Valladares <53414712+vnaj16@users.noreply.github.com>
2021-07-10 22:28:55 -0500

    Initial commit

---
+++ GestorCalificaciones.API/Context/GestorCalificacionesContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestorCalificaciones.API.Context
+{
+    public class GestorCalificacionesContext : DbContext
+    {
+        public GestorCalificacionesContext(DbContextOptions<GestorCalificacionesContext> options)
+            : base(options)
+        {
+        }
+    }
+}
